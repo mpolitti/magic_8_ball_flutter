b5c2c23b14152348895317040a9b29c2dfe301c8
Marco Politti <marcopolitti.br@gmail.com>
2020-07-16 07:44:06 -0300

    primeiro commit

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(BallPage());
+
+class BallPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
